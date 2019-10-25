2ab4be73268eb7404c4cff2006d4e8edbbd293cd
Nina <ninawu424@gmail.com>
2019-10-24 22:21:40 -0400

    Created HomeController, Index view and Recipe model

---
+++ RecipeSystem/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RecipeSystem.Models;
+
+namespace RecipeSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RecipeSystem/Models/Recipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeSystem.Models
+{
+    public class Recipe
+    {
+        public int RecipeID { get; set; }
+        public string DishName { get; set; }
+        public string Author { get; set; }
+        public string BriefIntro { get; set; }
+        public int PrepTime { get; set; }
+        public int Servings { get; set; }
+        public string Ingredients { get; set; }
+        public string Directions { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
